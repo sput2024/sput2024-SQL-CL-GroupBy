c8457b74a19b18144ccb5281be8bb94acf63d356
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-09-28 23:47:16 +0000

    Completed Coding Lab: Group By

--- problem1.sql
+++ problem1.sql
@@ -14,4 +14,5 @@
 -- HINT: You should avoid using the wild card (*) in your statement. All projected columns must either be used in the `GROUP BY` clause or an aggregate function.
 
 -- Write your sql statement anywhere below this line
+SELECT count(song), artist FROM song GROUP BY artist;
 
